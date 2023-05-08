20ee8a1abf6fcdf5e879e7f0a4de1d3fe3bc032c
maalvrei <miguemorisco@gmail.com>
2023-05-08 22:14:54 +0200

    ejercicio de arrays

---
+++ clasesDeMayo/src/main/java/ejercicioArrays/TestArray.java
@@ -0,0 +1,32 @@
+package ejercicioArrays;
+import java.util.Scanner;
+
+public class TestArray {
+
+	private final static int TAMANYO_ARRAY = 5;
+	static String[] arr = new String[TAMANYO_ARRAY];
+	
+	public static void llenaArray () {
+		Scanner sc = new Scanner(System.in);
+		for (int i = 0 ; i < TAMANYO_ARRAY ; i++) {
+			System.out.println("Introduce un elemento al array");
+			String e = sc.next();
+			arr[i] = e;
+		}
+		sc.close();
+	}
+	
+	public static void imprimeArray(String[] arra) {
+		int i = 0;
+		for (String s: arra) {
+			System.out.println("Elemento " + i + " del array.");
+			i++;
+		}
+	}
+	
+	public static void main(String[] args) {
+		llenaArray();
+		imprimeArray(arr);
+	}
+	
+}
