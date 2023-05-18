bb0a9b4003f7624d141f9495b38fae8e5467586d
unknown <miguemorisco@gmail.com>
2023-05-18 12:25:49 +0200

    corregido ejercicio 2 de strings

--- clasesDeMayo/src/main/java/ejerciciosConStrings/Ejercicio2.java
+++ clasesDeMayo/src/main/java/ejerciciosConStrings/Ejercicio2.java
@@ -4,25 +4,26 @@ import java.util.Scanner;
 
 public class Ejercicio2 {
 	
-	public static StringBuilder pideStringBuilder() {
+	public static String pideString() {
 		System.out.println("Introduce una cadena");
 		Scanner sc = new Scanner(System.in);
 		String s = sc.next();
 		sc.close();
-		StringBuilder sb = new StringBuilder(s);
-		return sb;
+		return s;
 	}
 	
-	public static String reverseCadena(StringBuilder s) {
-		String cadenaInvertida = s.reverse().toString();
+	public static String reverseString(String s) {
+		String cadenaInvertida = "";
+		for (int i = 0 ; i < s.length() ; i++)
+			cadenaInvertida += s.charAt(s.length() -1 - i);
 		return cadenaInvertida;
 	}
 	
 	public static void main(String[] args) {
 		
-		StringBuilder s = pideStringBuilder();
-		String cadenaInvertida = reverseCadena(s);
-		System.out.println("La cadena " + reverseCadena(s) + " del reves es " + cadenaInvertida);
+		String s = pideString();
+		String cadenaInvertida = reverseString(s);
+		System.out.println("La cadena " + s + " del reves es " + cadenaInvertida);
 		
 	}
 	
